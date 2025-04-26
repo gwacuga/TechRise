3f517b641448b44b6bc030f41aa0481dbb1e2192
gwacuga <wacugageorge@gmail.com>
2025-04-26 10:52:01 +0300

    Update HTML, CSS, JS files; add new images and remove old files

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
   
+  const images = [
+    "images/img1.jpg",
+    "images/img2.jpg",
+    "images/img3.jpg",
+    "images/img4.jpg",
+    "images/img5.jpg"
+  ];
+  
+  let currentIndex = 0;
+  const heroImage = document.getElementById("hero-image");
+  
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    heroImage.src = images[currentIndex];
+  }, 60000); // 60,000ms = 1 minute
+  
